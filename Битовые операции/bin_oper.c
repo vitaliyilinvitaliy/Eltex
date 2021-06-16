223d5a588790f84c6e2411dfe0dd5c014271e3e0
Vitaliy Ilin <vitaliyilin20001@gmail.com>
2021-06-16 10:11:39 +0700

    Bit oper

---
+++ Битовые операции/bin_oper.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+
+void print_bits_int(int digit){
+    printf("%#X - ",digit);
+
+    char count_bits = sizeof(int) * 8;
+    char i;
+
+    for(i = count_bits - 1; i >= 0 ; i--){
+        unsigned char bit = (digit >> i) & 1;
+        printf("%d",bit);
+        if(0 == (i % 8)){
+            printf(" ");
+        }
+    }
+    
+    printf("\n");
+}
+
+void print_bits(unsigned char digit){
+
+    printf("%X - ",digit);
+    char i;
+
+    for(i = 7; i >= 0 ; i--){
+        unsigned char bit = (digit >> i) & 0x1;
+        printf("%d",bit);
+    }
+    
+    printf("\n");
+}
+
+void print_bytes(int digit){
+    char count_bytes = sizeof(int);
+    char i;
+
+    for(i = count_bytes - 1; i >= 0 ; i--){
+        unsigned char b = (digit >> (i * 8)) & 0x000000FF;
+        print_bits(b);
+    }
+}
+
+unsigned char get_byte_int(int digit, unsigned char number_byte){
+    //функция получения байта целого числа, с индексом number_byte
+    unsigned char byte = (digit >> (number_byte * 8));
+    
+    return byte;
+}
+
+void replace_byte(int *digit ,unsigned char rep_byte, unsigned char number_byte){
+    int byte_position = 0;
+    int copy_digit = (*digit);
+
+    byte_position = ~(0xFF << (number_byte * 8));    
+    copy_digit = copy_digit & byte_position;
+
+    int insert_byte = rep_byte;
+    insert_byte = insert_byte  << (number_byte * 8);
+
+    copy_digit = copy_digit | insert_byte;
+
+    (*digit) = copy_digit;
+}
+
+
+int main(){
+    int a = 0x00AABBCC;
+
+    replace_byte(&a,0xDD,1);
+
+    print_bits_int(a);
+    print_bytes(a);
+}
