b4e91cab874b1b8ca319f538be4b984d2c534112
Vitaliy Ilin <vitaliyilin20001@gmail.com>
2021-07-20 00:35:35 +0700

    Sync

---
+++ Synchronization/task_mutex.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int a = 0;
+
+pthread_mutex_t m1;
+
+void *func(void *args){
+    int i = 0;
+    int tmp = 0;
+
+
+    for(i = 0; i < 1000; i++){
+        pthread_mutex_lock(&m1);
+        tmp = a;
+        tmp++;
+        a = tmp;
+        pthread_mutex_unlock(&m1);
+    }
+}
+
+int main(void){
+    
+    pthread_t tid[1000];
+
+    int ret = 0;
+
+    for(int i = 1; i <= 1000; i++){
+        ret = pthread_create(&tid[i - 1], NULL, func, NULL);
+        if(ret != 0){
+            perror("create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for(int i = 1; i <= 1000; i++){
+        ret = pthread_join(tid[i - 1], NULL);
+        if(ret != 0){
+            perror("join");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("a = %d\n",a);
+
+    return 0;
+}
